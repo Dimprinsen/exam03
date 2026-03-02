1a8afb52b4cbdb6b4fbe7c736c30b168823693e5
Thobias Tinnerholm <126627316+Dimprinsen@users.noreply.github.com>
2026-03-02 15:11:44 +0000

    wip

---
+++ gnl/gnl_test1.c
@@ -0,0 +1,61 @@
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char *get_next_line(int fd)
+{
+    static char buffer[BUFFER_SIZE];
+    static int buffer_index = 0;
+    static int read_bytes = 0;
+    char *line = NULL;
+    int line_len = 0;
+    char *temp;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return NULL;
+
+    while (1)
+    {
+        if (buffer_index >= read_bytes)
+        {
+            read_bytes = read(fd, buffer, BUFFER_SIZE);
+            if (read_bytes <= 0)
+                break;
+            buffer_index = 0;
+        }
+        temp = malloc(line_len + 2); //for current char and null terminator
+        if (!temp)
+        {
+            free(line);
+            return NULL;
+        }
+        int i = 0;
+        while (i < line_len) 
+        {
+            temp[i] = line[i];
+            i++;
+        }
+        temp[line_len] = buffer[buffer_index];
+        temp[line_len + 1] = '\0';
+        free(line);
+        line = temp;
+        line_len ++;
+
+        if (buffer[buffer_index] == '\n')
+        {
+            buffer_index ++;
+            return (line);
+        }
+        buffer_index ++;
+    }
+    if (line_len == 0)
+    {
+        free(line);
+        return NULL;
+    }
+    return (line);
+}
