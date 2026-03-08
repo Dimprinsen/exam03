47602206fef6f31252cb1c3a2ad9c939392923c3
Thobias Tinnerholm <126627316+Dimprinsen@users.noreply.github.com>
2026-03-08 23:49:24 +0000

    changed power set

--- powerset/powerset.c
+++ powerset/powerset.c
@@ -3,32 +3,49 @@
 
 void powerset(int *a, int n, int target, int *sub, int size, int i, int sum)
 {
+    int j;
+
     if (sum == target)
     {
-        for (int j = 0; j < size; j++)
+        j = 0;
+        while (j < size)
         {
             if (j)
                 printf(" %d", sub[j]);
             else
                 printf("%d", sub[j]);
+            j++;
         }
         printf("\n");
     }
 
-    for (; i < n; i++)
+    while (i < n)
     {
         sub[size] = a[i];
         powerset(a, n, target, sub, size + 1, i + 1, sum + a[i]);
+        i++;
     }
 }
 
 int main(int argc, char **argv)
 {
+    int target;
+    int n = argc - 2;
+    int a[100];
+    int sub[100];
+    int i = 0;
+
     if (argc < 3)
         return 1;
-    int target = atoi(argv[1]), n = argc - 2, a[100], sub[100];
-    for (int i = 0; i < n; i++)
+
+    target = atoi(argv[1]);
+
+    while (i < n)
+    {
         a[i] = atoi(argv[i + 2]);
+        i++;
+    }
+
     powerset(a, n, target, sub, 0, 0, 0);
     return 0;
 }
